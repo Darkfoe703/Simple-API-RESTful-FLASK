9181b51b260fc77c5b372f653c51b87d1d495285
Marco Romero <marcoromero_1@hotmail.com>
2024-09-02 21:54:53 -0300

    Update model

--- models.py
+++ models.py
@@ -1,6 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
+from enum import Enum
 
 db = SQLAlchemy()
+
+
+class StatusEnum(Enum):
+    UNASSIGNED = "Unassigned"
+    ASSIGNED = "Assigned"
+    PENDING = "Pending"
+    COMPLETED = "Completed"
+    IN_PROGRESS = "In Progress"
+
 
 class Employee(db.Model):
     __tablename__ = 'employees'
@@ -14,4 +24,5 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(200))
-    employee_id = db.Column(db.Integer, db.ForeignKey("employees.id"), nullable=False)
+    employee_id = db.Column(db.Integer, db.ForeignKey("employees.name"), nullable=False)
+    status = db.Column(db.Enum(StatusEnum), nullable=False, default=StatusEnum.UNASSIGNED)
