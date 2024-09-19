5df06ca078891689182b72618d1ca3484b125d29
Marco Romero <marcoromero_1@hotmail.com>
2024-09-19 17:30:49 -0300

    update DB settings

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv(
         "DATABASE_URL",
-        "postgresql+psycopg2://postgres:vaallover15@localhost:5432/task_api_db",
+        "postgresql+psycopg2://postgres:pass1234@localhost:5432/task_api_db",
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
