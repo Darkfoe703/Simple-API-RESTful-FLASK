7e5fcad4b2dd0bff26ef2e9c0e7b987c4bd83357
Marco Romero <marcoromero_1@hotmail.com>
2024-11-22 20:59:16 -0300

    Set a .env file --> app and docker-compose

--- .flaskenv
+++
@@ -1,5 +0,0 @@
-FLASK_ENV=development
-
-FLASK_DEBUG=True
-
-FLASK_APP=app.py
--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv(
         "DATABASE_URL",
-        "postgresql+psycopg2://postgres:pass1234@localhost:5432/task_api_db",
+        "DATABASE_URL",
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
